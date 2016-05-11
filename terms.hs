b3840261ef94d499726a85acab0ef4743d1771fd
U-DESKTOP-EB8ITR4\David Atias <davidalejandro23@gmail.com>
2016-05-10 23:02:40 -0400

    Definición de Term y show copy-paste de Flaviani

--- terms.hs
+++ terms.hs
@@ -1,78 +1,142 @@
+data Term = T
+		  | F
+		  | Var String
+		  | Not Term 
+		  | Or Term Term 
+		  | And Term Term 
+		  | Impl Term Term 
+		  | Iff Term Term 
+		  | Niff Term Term
+
+
+(\/) :: Term -> Term -> Term
+(\/) t1 t2 = Or t1 t2
+
+(/\) :: Term -> Term -> Term
+(/\) t1 t2 = And t1 t2
+
+(==>) :: Term -> Term -> Term
+(==>) t1 t2 = Impl t1 t2
+
+(<==>) :: Term -> Term -> Term
+(<==>) t1 t2 = Iff t1 t2
+
+(!<==>) :: Term -> Term -> Term
+(!<==>) t1 t2 = Niff t1 t2
+
+showTerm :: Term -> String
+
+showTerm (Var i) = id i
+
+showTerm (Not t) = "!" ++ showTerm t
+
+showTerm T = "true"
+showTerm F = "false"
+
+showTerm (Or (Var i) (Var j)) = showTerm(Var i) ++ " \\/ " ++ showTerm(Var j)
+showTerm (Or (Var i) t) = showTerm(Var i) ++ " \\/ (" ++ showTerm(t) ++ ")"
+showTerm (Or t (Var i)) = "(" ++ showTerm(t) ++ ")" ++ " \\/ " ++ showTerm(Var i)
+showTerm (Or t1 t2) = "(" ++ showTerm t1 ++ ") \\/ (" ++ showTerm t2 ++ ")"
+
+showTerm (And (Var i) (Var j)) = showTerm(Var i) ++ " /\\ " ++ showTerm(Var j)
+showTerm (And (Var i) t) = showTerm(Var i) ++ " /\\ (" ++ showTerm(t) ++ ")"
+showTerm (And t (Var i)) = "(" ++ showTerm(t) ++ ")" ++ " /\\ " ++ showTerm(Var i)
+showTerm (And t1 t2) = "(" ++ showTerm t1 ++ ") /\\ (" ++ showTerm t2 ++ ")"
+
+showTerm (Impl (Var i) (Var j)) = showTerm(Var i) ++ " ==> " ++ showTerm(Var j)
+showTerm (Impl (Var i) t) = showTerm(Var i) ++ " ==> (" ++ showTerm(t) ++ ")"
+showTerm (Impl t (Var i)) = "(" ++ showTerm(t) ++ ")" ++ " ==> " ++ showTerm(Var i)
+showTerm (Impl t1 t2) = "(" ++ showTerm t1 ++ ") ==> (" ++ showTerm t2 ++ ")"
+
+showTerm (Iff (Var i) (Var j)) = showTerm(Var i) ++ " <==> " ++ showTerm(Var j)
+showTerm (Iff (Var i) t) = showTerm(Var i) ++ " <==> (" ++ showTerm(t) ++ ")"
+showTerm (Iff t (Var i)) = "(" ++ showTerm(t) ++ ")" ++ " <==> " ++ showTerm(Var i)
+showTerm (Iff t1 t2) = "(" ++ showTerm t1 ++ ") <==> (" ++ showTerm t2 ++ ")"
+
+showTerm (Niff (Var i) (Var j)) = showTerm(Var i) ++ " !<==> " ++ showTerm(Var j)
+showTerm (Niff (Var i) t) = showTerm(Var i) ++ " !<==> (" ++ showTerm(t) ++ ")"
+showTerm (Niff t (Var i)) = "(" ++ showTerm(t) ++ ")" ++ " !<==> " ++ showTerm(Var i)
+showTerm (Niff t1 t2) = "(" ++ showTerm t1 ++ ") !<==> (" ++ showTerm t2 ++ ")"
+
+
+
+instance Show Term where show = showTerm
+
 a :: Term
-a = Var 'a'
+a = Var "a"
 
 b :: Term
-b = Var 'b'
+b = Var "b"
 
 c :: Term
-c = Var 'c'
+c = Var "c"
 
 d :: Term
-d = Var 'd'
+d = Var "d"
 
 e :: Term
-e = Var 'e'
+e = Var "e"
 
 f :: Term
-f = Var 'f'
+f = Var "f"
 
 g :: Term
-g = Var 'g'
+g = Var "g"
 
 h :: Term
-h = Var 'h'
+h = Var "h"
 
 i :: Term
-i = Var 'i'
+i = Var "i"
 
 j :: Term
-j = Var 'j'
+j = Var "j"
 
 k :: Term
-k = Var 'k'
+k = Var "k"
 
 l :: Term
-l = Var 'l'
+l = Var "l"
 
 m :: Term
-m = Var 'm'
+m = Var "m"
 
 n :: Term
-n = Var 'n'
+n = Var "n"
 
 o :: Term
-o = Var 'o'
+o = Var "o"
 
 p :: Term
-p = Var 'p'
+p = Var "p"
 
 q :: Term
-q = Var 'q'
+q = Var "q"
 
 r :: Term
-r = Var 'r'
+r = Var "r"
 
 s :: Term
-s = Var 's'
+s = Var "s"
 
 t :: Term
-t = Var 't'
+t = Var "t"
 
 u :: Term
-u = Var 'u'
+u = Var "u"
 
 v :: Term
-v = Var 'v'
+v = Var "v"
 
 w :: Term
-w = Var 'w'
+w = Var "w"
 
 x :: Term
-x = Var 'x'
+x = Var "x"
 
 y :: Term
-y = Var 'y'
+y = Var "y"
 
 z :: Term
-z = Var 'z'
+z = Var "z"
 
