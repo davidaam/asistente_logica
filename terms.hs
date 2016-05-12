66becaba91b769624fefee105dbfbcce6a2322ba
U-DESKTOP-EB8ITR4\David Atias <davidalejandro23@gmail.com>
2016-05-11 22:01:22 -0400

    Sustitución textual paralela funcionando

--- terms.hs
+++ terms.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 data Term = T
 		  | F
 		  | Var String
@@ -28,7 +30,10 @@ showTerm :: Term -> String
 
 showTerm (Var i) = id i
 
-showTerm (Not t) = "!" ++ showTerm t
+showTerm (Not (Var i)) = "!" ++ showTerm t
+showTerm (Not T) = "!" ++ showTerm T
+showTerm (Not F) = "!" ++ showTerm F
+showTerm (Not t) = "!(" ++ showTerm t ++ ")"
 
 showTerm T = "true"
 showTerm F = "false"
@@ -57,8 +62,6 @@ showTerm (Niff (Var i) (Var j)) = showTerm(Var i) ++ " !<==> " ++ showTerm(Var j
 showTerm (Niff (Var i) t) = showTerm(Var i) ++ " !<==> (" ++ showTerm(t) ++ ")"
 showTerm (Niff t (Var i)) = "(" ++ showTerm(t) ++ ")" ++ " !<==> " ++ showTerm(Var i)
 showTerm (Niff t1 t2) = "(" ++ showTerm t1 ++ ") !<==> (" ++ showTerm t2 ++ ")"
-
-
 
 instance Show Term where show = showTerm
 
@@ -140,3 +143,71 @@ y = Var "y"
 z :: Term
 z = Var "z"
 
+data Equation = Equation Term Term
+
+instance Eq Term where
+	Var t1 == Var t2 = t1 == t2
+	T == T = True
+	F == F = True
+	(Or t1 t2) == (Or t3 t4) = (t1 == t3) && (t2 == t4)
+	(And t1 t2) == (And t3 t4) = (t1 == t3) && (t2 == t4)
+	(Impl t1 t2) == (Impl t3 t4) = (t1 == t3) && (t2 == t4)
+	(Iff t1 t2) == (Iff t3 t4) = (t1 == t3) && (t2 == t4)
+	(Niff t1 t2) == (Niff t3 t4) = (t1 == t3) && (t2 == t4)
+	(Not t1) == (Not t2) = t1 == t2
+	_ == _ = False
+
+instance Eq Equation where
+	Equation t1 t2 == Equation t3 t4
+		| t1 == t3 && t2 == t4 = True
+		| t1 == t4 && t2 == t3 = True
+		| otherwise = False
+
+instance Show Equation where
+	show (Equation t1 t2) = show t1 ++ " === " ++ show t2
+
+-- Asumimos que la entrada es correcta
+data Sust = Sust Term Term
+
+-- Abstraer de Flaviani
+
+ci = \x -> x
+ck = \x -> \y -> x
+cs = \x -> \y -> \z -> (x z) (y z)
+
+abstraer :: Term -> Term -> (Term -> Term)
+abstraer (Var x) (Var y) = if x == y then ci else ck (Var y)
+abstraer (Var x) (Or t1 t2) = cs (cs (ck Or) (abstraer (Var x) t1)) (abstraer (Var x) t2)
+abstraer (Var x) (And t1 t2) = cs (cs (ck And) (abstraer (Var x) t1)) (abstraer (Var x) t2)
+abstraer (Var x) (Impl t1 t2) = cs (cs (ck Impl) (abstraer (Var x) t1)) (abstraer (Var x) t2)
+abstraer (Var x) (Iff t1 t2) = cs (cs (ck Iff) (abstraer (Var x) t1)) (abstraer (Var x) t2)
+abstraer (Var x) (Niff t1 t2) = cs (cs (ck Niff) (abstraer (Var x) t1)) (abstraer (Var x) t2)
+
+abstraer (Or t1 t2) _ = error "Solo se puede abstraer una variable"
+
+
+class Instantiable s where
+	sustituir :: Term -> s -> Term
+	sustVar :: Term -> s -> Term
+	instantiate :: Equation -> s -> Equation
+	instantiate (Equation t1 t2) sus = Equation (sustituir t1 sus) (sustituir t2 sus)
+
+	-- Casos recursivos
+	sustituir (Or t1 t2) sus = Or (sustituir t1 sus) (sustituir t2 sus)
+	sustituir (And t1 t2) sus = And (sustituir t1 sus) (sustituir t2 sus)
+	sustituir (Impl t1 t2) sus = Impl (sustituir t1 sus) (sustituir t2 sus)
+	sustituir (Iff t1 t2) sus = Iff (sustituir t1 sus) (sustituir t2 sus)
+	sustituir (Niff t1 t2) sus = Niff (sustituir t1 sus) (sustituir t2 sus)
+	-- Casos base
+	sustituir T _ = T
+	sustituir F _ = F
+	sustituir (Var v1) sus = sustVar (Var v1) sus
+
+instance Instantiable Sust where
+	sustVar (Var v1) (Sust sustitucion variable) = (abstraer variable (Var v1)) sustitucion
+
+instance Instantiable (Term, Sust, Term) where
+	sustVar (Var v1) (t1, Sust t2 s1, s2)
+		| (Var v1) == s1 = (abstraer (Var v1) (Var v1)) t1
+		| (Var v1) == s2 = (abstraer (Var v1) (Var v1)) t2
+		| otherwise = Var v1
